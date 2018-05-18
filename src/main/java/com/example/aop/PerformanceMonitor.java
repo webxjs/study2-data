9178fe597b9451cca7f1112998bf69d3c6850c66
yangqijun <wealther@163.com>
2018-05-17 22:08:29 -0500

    add performance monitor

--- src/main/java/com/example/aop/PerformanceMonitor.java
+++ src/main/java/com/example/aop/PerformanceMonitor.java
@@ -3,7 +3,10 @@ package com.example.aop;
 import java.util.Arrays;
 import java.util.Date;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
@@ -11,19 +14,20 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class PerformanceMonitor{
+	protected Log log = LogFactory.getLog(PerformanceMonitor.class);
 	@Around("execution(* com.example.service..*(..))")
 	public Object collectExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
-		Date date = new Date();
-		System.out.println("The method " + joinPoint.getSignature().getName() + "() begins with " + Arrays.toString(joinPoint.getArgs()));
+        Signature signature = joinPoint.getSignature();
 	    try
 	    {
+	    	long startTime = new Date().getTime();
 	        Object result = joinPoint.proceed();
-	        System.out.println("The method " + joinPoint.getSignature().getName() + "() consume time " + (new Date().getTime() - date.getTime()) + " ms");
+	        log.warn(joinPoint.getTarget().getClass() + "." + signature.getName() + " consumes " + (new Date().getTime() - startTime) + " ms");
 	        return result;
 	    }
 	    catch (IllegalArgumentException e)
 	    {
-	    	System.out.println("Illegal argument " + Arrays.toString(joinPoint.getArgs()) + " in " + joinPoint.getSignature().getName() + "()");
+	    	log.warn("Illegal argument " + Arrays.toString(joinPoint.getArgs()) + " in " + signature.getName() + "()");
 	        throw e;
 	    }
    }
